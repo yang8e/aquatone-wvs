e084e9e442989bf7db84dd48c88848a572653bef
yang8e <yangxiaocheng@live.cn>
2018-09-06 17:49:29 +0800

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-def aquatone_scan(target):
-    os.system('aquatone-discover -d '+target)
-    os.system('aquatone-scan -p huge -d '+target)
-
-input_url = sys.argv[1]
-aquatone_scan(input_url)
